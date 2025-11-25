1215ecff8c6441dd8e41707d2cced89e4cb95cd5
kalpesh <kalpesh162@gmail.com>
2025-11-25 11:04:09 +0530

    component mapping all

--- ComponentMapping/src/com/dao/EmployeeDao.java
+++ ComponentMapping/src/com/dao/EmployeeDao.java
@@ -1,9 +1,19 @@
 package com.dao;
 
+import java.util.List;
+
 import com.entity.Employee;
 
 public interface EmployeeDao {
-	
+
 	void saveEmployee(Employee employee);
+
+	void updateEmployee(Employee employee);
+
+	void deleteEmployee(Employee employee);
+
+	Employee getEmployeeById(Integer id);
+
+	List<Employee> getAllEmployee();
 
 }
--- ComponentMapping/src/com/dao/EmployeeDaoImpl.java
+++ ComponentMapping/src/com/dao/EmployeeDaoImpl.java
@@ -1,5 +1,7 @@
 package com.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -23,6 +25,59 @@ public class EmployeeDaoImpl implements EmployeeDao {
 			throw e;
 		}
 
+	}
+
+	public void updateEmployee(Employee employee) {
+		Transaction tx = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+			tx = session.beginTransaction();
+			session.update(employee); //
+			tx.commit();
+
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		}
+
+	}
+
+	public void deleteEmployee(Employee employee) {
+		Transaction tx = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+			tx = session.beginTransaction();
+			session.delete(employee); //
+			tx.commit();
+
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		}
+
+	}
+
+	public Employee getEmployeeById(Integer id) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+			return session.get(Employee.class, id); //
+
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public List<Employee> getAllEmployee() {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			String hql = "from Employee e";
+
+			return session.createQuery(hql, Employee.class).list();
+
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 }
